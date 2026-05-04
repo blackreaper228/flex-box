dbd569c8a28e210cc4914ed1204202b1b6db3ce3
Nikolai <ntsvetnikov@gmail.com>
2026-05-04 15:28:54 +0300

    errors_fixed

--- src/main.js
+++ src/main.js
@@ -1,9 +1,7 @@
 import './style.css';
-import './js/slider.js';
 import './js/accordion.js';
 import './js/textSlider.js';
-import './js/customScroll.js'; 
-
+import './js/customScroll.js';
 
 const DESIGN_WIDTH = 1440;
 const MIN_SCALE_BREAKPOINT = 769; // start scaling at >= 768px viewport width
